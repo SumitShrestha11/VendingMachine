1295dc5d7163608f2a503f76f64998cee6c9c820
Sumit <sumit.sth54@gmail.com>
2024-12-09 22:31:25 +0545

    chore: added cors middleware

--- vending-machine-backend/src/app/index.ts
+++ vending-machine-backend/src/app/index.ts
@@ -1,9 +1,11 @@
+const cors = require("cors");
 import express from "express";
 import vendingMachineRoutes from "./http/routes/VendingMachineRoutes";
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 app.use("/api/v1/vending-machine", vendingMachineRoutes);
 
 export default app;
